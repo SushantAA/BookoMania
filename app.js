246caa5bb81fcc02e76c9c1e8fdd44504d9cc26d
Tanmay Arora <67386710+tanmayarora11@users.noreply.github.com>
2021-03-20 01:39:09 +0530

    npm init

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(express.json() );       // to support JSON-encoded bodies
+app.use(express.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
