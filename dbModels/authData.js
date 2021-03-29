53ab0641357951c133779cc38687b192bf0aac27
Tanmay Arora <67386710+tanmayarora11@users.noreply.github.com>
2021-03-29 19:05:40 +0530

    merged conflicts

--- dbModels/authData.js
+++ dbModels/authData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -15,29 +14,3 @@ const userSchema = new Schema({
 
 module.exports = mongoose.model('User',userSchema);
 
-=======
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
-
-// module.exports = mongoose.model('User', {
-//     name: String,
-//     password: String
-// });
-
-const userSchema = new Schema({
-    name:{
-        type:String,
-        require:true
-    },
-    password:{
-        type:String,
-        require:true
-    }
-});
-
-userSchema.plugin(passportLocalMongoose);
-
-module.exports = mongoose.model('User',userSchema);
-
->>>>>>> f2c1f09bc077b27790ef1b9b84973524df3042f3
