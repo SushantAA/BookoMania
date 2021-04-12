5217e15e72e9acd795fa07eb63ed9be73df5f83c
Tanmay Arora <67386710+tanmayarora11@users.noreply.github.com>
2021-04-12 10:40:38 +0530

    UI design

--- new/public/scripts/form_validate.js
+++ new/public/scripts/form_validate.js
@@ -1,23 +1,21 @@
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  'use strict'
+'use strict'
 
-  bsCustomFileInput.init();
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation')
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.new-edit-validation')
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+.forEach(function (form) {
+  form.addEventListener('submit', function (event) {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
+    form.classList.add('was-validated')
+  }, false)
+})
 })()
